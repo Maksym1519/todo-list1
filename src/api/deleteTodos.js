9e6826085f9a2dc41e855a326f45637150852d70
Maksym Korostelov <flying1519@gmail.com>
2025-05-12 20:12:20 +0300

    changed bg-todolist dark theme

--- src/api/deleteTodos.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const deleteTodos = async (id) => {
-    const response = await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    return response.data;
-  };
--- src/api/getTodos.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getTodos = async () => {
-    const response = await axios.get(`${'https://jsonplaceholder.typicode.com/todos?_limit=10'}?_limit=10`);
-    return response.data;
-  };
